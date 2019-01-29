b1cd8176ce9b6c6a09daaf35d4c8a4738ef5478b
Jacob Olsson <jacob.n.olsson@hotmail.com>
2019-01-29 08:16:27 +0100

    added 2_7

--- P2/exerc_2_7.c
+++ P2/exerc_2_7.c
@@ -2,7 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
+void readPersnr( char *person){
+    printf("Input person number: ");
+    scanf("%[^\n]%*c", person);
+}
+
+int controlDigit( const char * persnr ){
+    int nDigits = strlen(persnr);
+    int nSum = 0, isSecond = 0;
+
+    for (int i = nDigits - 1; i >= 0; i--) {
+
+        int d = persnr[i] - '0';
+
+        if (isSecond == 1)
+            d = d * 2;
+
+        nSum += d / 10;
+        nSum += d % 10;
+
+        isSecond = !isSecond;
+    }
+    return (nSum % 10 == 0);
+}
+
 int main(int argc, char *argv[]){
 
+    char personnr[20];
+
+    readPersnr(personnr);
+
+    printf("Person number is correct : %d \n" , controlDigit(personnr));
 
 }
