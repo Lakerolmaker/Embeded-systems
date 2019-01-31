f71e4d6ad69fe7de31f70b7e0b32cda4c671c04d
VicRoman97 <38137876+VicRoman97@users.noreply.github.com>
2019-01-31 19:18:46 +0100

    XD

--- P2/exerc_2_4.c
+++ P2/exerc_2_4.c
@@ -1,8 +1,26 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-int main(int argc, char *argv[]){
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
 
 
-}
+int main() {
+   
+
+        char input[255];
+        printf("insert palindrome");
+        scanf("%s", &input);
+        char flipped[255];
+        int end = strlen(input) - 1;
+        int middle = strlen(input)/2;
+        int i = 0;
+
+        for (i; i<middle; i++ ) {
+            if (input[i] != input[end])
+            {printf("woopsie"); break;}
+            end--;
+        }
+        //once you reach the middle
+        if (i == middle)
+        { printf("yay");}
+    }
+
