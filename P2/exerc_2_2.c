d36372ab52096c27e10251e0ac9e1b7710bac5be
VicRoman97 <38137876+VicRoman97@users.noreply.github.com>
2019-02-07 10:17:28 +0100

    Update exerc_2_2.c

--- P2/exerc_2_2.c
+++ P2/exerc_2_2.c
@@ -4,26 +4,27 @@
 
 #define MAX 100
 
-
+       //random number generator
 int random_number(int min_num, int max_num){
     return(rand() % ((max_num ) - min_num) + 1) + min_num;
 }
-
+    //get byte function
 int byteSize(const int *input[]){
   return sizeof(input);
 }
 
 
 int main(void){
+    //seed random generator
   srand(time(0));
-
+    
   int array[100];
-
+    //populate array with random numbers
   int i;
   for(i = 0; i < MAX; i++){
     array[i] = random_number(1,99);
   }
-
+    
   printf("The value of the label array( address ) is: %p \n", &array);
   printf("First integer in the array is ( array[0] ) is : %d \n", array[0]);
   printf("The size of an integer (number of bytes) is : %ld \n", sizeof(array));
