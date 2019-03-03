5d96aba270ac6d7ec8d5a14f954878d18e0cb8f0
VicRoman97 <38137876+VicRoman97@users.noreply.github.com>
2019-03-03 17:09:25 +0100

    Update 6_3alt.c

--- 6_3alt.c
+++ 6_3alt.c
@@ -1,7 +1,6 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
 
+#include <stdlib.h>
+#include <stdio.h>
 
 #define BIT_SET(a,b) ((a) |= (1ULL<<(b)))
 #define BIT_CLEAR(a,b) ((a) &= ~(1ULL<<(b)))
@@ -11,42 +10,7 @@
 #define ENTRY_STATE entry
 #define statusPORT 0x0B00
 #define controlPORT 0x0B00
-
-
-*portopen = B10000000;
-*portclose = B01000000;
-
 int bits;
-//close doors
-int entry_state(void);
-//read state
-int read_state(void);
-//open state
-int open_state(void);
-
-int exit_state(void);
-
-int (* state[])(void) = { entry_state, read_state, open_state, exit_state};
-enum state_codes { entry, read, open, end};
-
-enum ret_codes { ok, fail, repeat};
-struct transition {
-    enum state_codes src_state;
-    enum ret_codes   ret_code;
-    enum state_codes dst_state;
-};
-struct transition state_transitions[] = {
-    {entry, ok,     read},
-    {entry, fail,   end},
-    {read,   ok,     open},
-    {read,   fail,   end},
-    {read,   repeat, read},
-    {open,   ok,     end},
-    {open,   fail,   end},
-    {open,   repeat, read}};
-
-
-
 void setBit(int *bits, int index, int b){
     if(b)
         BIT_SET(*bits, index);
@@ -54,38 +18,84 @@ void setBit(int *bits, int index, int b){
         BIT_CLEAR(*bits, index);
 }
 
+enum states {
+	START,
+	LOOP,
+	END,
+} state;
 
-entry_state(void) {
-  while (statusPORT != "B00100000") {
-  setBit(&PORT, 7, BIT_CHECK(bits ,1);)
+enum events {
+	START_LOOPING,
+	READ,
+	OPEN,
+	STOP_LOOPING,
+};
+
+void step_state(enum events event) {
+	int flag=0;
+
+	switch(state) {
+	case START:
+		switch(event) {
+		case START_LOOPING:
+		entry_state();
+			state = LOOP;
+			break;
+		default:
+			exit(1);
+			break;
+		}
+		break;
+
+	case LOOP:
+		switch(event) {
+		case READ:
+ 		read_state();
+			break;
+
+		case OPEN:
+			open_state();
+			break;
+
+		case STOP_LOOPING:
+			state = END;
+			break;
+
+		default:
+			exit(1);
+			break;
+		}
+		break;
+	case END:
+		exit(1);
+		break;
+	}
 }
 
-}
+	entry_state(void) {
+//	  while (statusPORT != "B00100000") {
+//	  setBit(statusPORT, 7, BIT_CHECK(bits ,1));
+//		printf("start\n");
+//	}
+	printf("start\n");
+	}
 
-read_state(void) {
-    do{   setBit(&PORT, 8, BIT_CHECK(bits ,1);) }
-  while (statusPORT == "B00000001" || statusPORT == "B00000010" )
-
-
-}
-
-int main (void)
-{
-  enum state_codes cur_state = ENTRY_STATE;
-     enum ret_codes rc;
-     int (* state_fun)(void);
-
-     for (;;) {
-         state_fun = state[cur_state];
-         rc = state_fun();
-         if (EXIT_STATE == cur_state)
-             break;
-             //lookup has to be implemented
-         cur_state = lookup_transitions(cur_state, rc);
-     }
-
- return EXIT_SUCCESS;
+	read_state(void) {
+		printf("wait for button!\n"); //keep reading
+		 		printf("button pressed!\n"); //detect button
 
 
+	}
 
+	open_state() {
+			printf("opening!\n");
+			printf("waiting!\n");
+			printf("closing!\n");
+	}
+
+int main(void) {
+	step_state(START_LOOPING);
+	step_state(READ);
+  step_state(OPEN);
+	return 0;
 }
